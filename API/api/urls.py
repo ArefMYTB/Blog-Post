b8c234df8020d0625874205bc0313f28507b182f
Aref TB <92437542+ArefMYTB@users.noreply.github.com>
2024-07-21 20:34:23 +0330

    Add login&register

--- API/api/urls.py
+++ API/api/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 
 urlpatterns = [
+    path("login", views.loginPage, name='login'),
+    path("logout", views.logoutUser, name='logout'),
+    path("register", views.registerPage, name='register'),
     path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
     path("blogposts/<int:pk>", views.BlogPostRetrieveUpdateDestroy.as_view(), name="update"),
     path("blogposts/list/", views.BlogPostList.as_view(), name="show")
